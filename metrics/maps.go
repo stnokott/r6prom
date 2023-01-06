a7667c3675904a66e7d78a4e0c44df48fd298215
stnokott <nftrash@web.de>
2023-01-06 01:32:51 +0100

    fix: provide map stats under dedicated measurement with correct data

--- metrics/maps.go
+++ metrics/maps.go
@@ -17,69 +17,64 @@ func SendMapStats(api *r6api.R6API, profile *r6api.Profile, meta *metadata.Metad
 		return
 	}
 
-	gameModes := map[string]*stats.NamedTeamRoles{
-		"all":      mapStats.All,
-		"casual":   mapStats.Casual,
-		"unranked": mapStats.Unranked,
-		"ranked":   mapStats.Ranked,
+	gameModes := map[string]map[string]stats.NamedMapStatDetails{
+		"all":      *mapStats.All,
+		"casual":   *mapStats.Casual,
+		"unranked": *mapStats.Unranked,
+		"ranked":   *mapStats.Ranked,
 	}
 
 	for gameModeName, gameModeStats := range gameModes {
-		roles := map[string]stats.NamedTeamRoleStats{
-			"all":     gameModeStats.All,
-			"attack":  gameModeStats.Attack,
-			"defence": gameModeStats.Defence,
-		}
-		for roleName, roleStats := range roles {
-			for mapName, mapStats := range roleStats {
-				chData <- StatResponse{
-					P: influxdb2.NewPoint(
-						"actions",
-						map[string]string{
-							"season_slug": currentSeason.Slug,
-							"season_name": currentSeason.Name,
-							"username":    profile.Name,
-							"gamemode":    gameModeName,
-							"role":        roleName,
-							"map":         mapName,
-						},
-						map[string]interface{}{
-							"kills":                   mapStats.Kills,
-							"deaths":                  mapStats.Deaths,
-							"assists":                 mapStats.Assists,
-							"melee_kills":             mapStats.MeleeKills,
-							"team_kills":              mapStats.TeamKills,
-							"trades":                  mapStats.Trades,
-							"revives":                 mapStats.Revives,
-							"headshots":               mapStats.Headshots,
-							"rounds_played":           mapStats.RoundsPlayed,
-							"rounds_won":              mapStats.RoundsWon,
-							"rounds_lost":             mapStats.RoundsLost,
-							"minutes_played":          mapStats.MinutesPlayed,
-							"kills_per_round":         mapStats.KillsPerRound,
-							"headshot_percentage":     mapStats.HeadshotPercentage,
-							"entry_deaths":            mapStats.EntryDeaths,
-							"entry_death_trades":      mapStats.EntryDeathTrades,
-							"entry_kills":             mapStats.EntryKills,
-							"entry_kill_trades":       mapStats.EntryKillTrades,
-							"rounds_survived":         mapStats.RoundsSurvived,
-							"rounds_with_kill":        mapStats.RoundsWithKill,
-							"rounds_with_multikill":   mapStats.RoundsWithMultikill,
-							"rounds_with_ace":         mapStats.RoundsWithAce,
-							"rounds_with_clutch":      mapStats.RoundsWithClutch,
-							"rounds_with_kost":        mapStats.RoundsWithKOST,
-							"rounds_with_entry_death": mapStats.RoundsWithEntryDeath,
-							"rounds_with_entry_kill":  mapStats.RoundsWithEntryKill,
-							"distance_per_round":      mapStats.DistancePerRound,
-							"distance_total":          mapStats.DistanceTotal,
-							"time_alive_per_match":    mapStats.TimeAlivePerMatch,
-							"time_dead_per_match":     mapStats.TimeDeadPerMatch,
-						},
-						t,
-					),
-				}
-
+		for mapName, mapStats := range gameModeStats {
+			chData <- StatResponse{
+				P: influxdb2.NewPoint(
+					"maps",
+					map[string]string{
+						"season_slug": currentSeason.Slug,
+						"season_name": currentSeason.Name,
+						"username":    profile.Name,
+						"gamemode":    gameModeName,
+						"map":         mapName,
+					},
+					map[string]interface{}{
+						"matches_played":          mapStats.MatchesPlayed,
+						"matches_won":             mapStats.MatchesWon,
+						"matches_lost":            mapStats.MatchesLost,
+						"kills":                   mapStats.Kills,
+						"deaths":                  mapStats.Deaths,
+						"assists":                 mapStats.Assists,
+						"melee_kills":             mapStats.MeleeKills,
+						"team_kills":              mapStats.TeamKills,
+						"trades":                  mapStats.Trades,
+						"revives":                 mapStats.Revives,
+						"headshots":               mapStats.Headshots,
+						"rounds_played":           mapStats.RoundsPlayed,
+						"rounds_won":              mapStats.RoundsWon,
+						"rounds_lost":             mapStats.RoundsLost,
+						"minutes_played":          mapStats.MinutesPlayed,
+						"kills_per_round":         mapStats.KillsPerRound,
+						"headshot_percentage":     mapStats.HeadshotPercentage,
+						"entry_deaths":            mapStats.EntryDeaths,
+						"entry_death_trades":      mapStats.EntryDeathTrades,
+						"entry_kills":             mapStats.EntryKills,
+						"entry_kill_trades":       mapStats.EntryKillTrades,
+						"rounds_survived":         mapStats.RoundsSurvived,
+						"rounds_with_kill":        mapStats.RoundsWithKill,
+						"rounds_with_multikill":   mapStats.RoundsWithMultikill,
+						"rounds_with_ace":         mapStats.RoundsWithAce,
+						"rounds_with_clutch":      mapStats.RoundsWithClutch,
+						"rounds_with_kost":        mapStats.RoundsWithKOST,
+						"rounds_with_entry_death": mapStats.RoundsWithEntryDeath,
+						"rounds_with_entry_kill":  mapStats.RoundsWithEntryKill,
+						"distance_per_round":      mapStats.DistancePerRound,
+						"distance_total":          mapStats.DistanceTotal,
+						"time_alive_per_match":    mapStats.TimeAlivePerMatch,
+						"time_dead_per_match":     mapStats.TimeDeadPerMatch,
+					},
+					t,
+				),
 			}
+
 		}
 	}
 	chData <- StatResponse{Done: true}
