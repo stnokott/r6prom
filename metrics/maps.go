78cdac320b217c5cd0255bb4c99ac539dc930139
stnokott <nftrash@web.de>
2023-01-08 03:28:01 +0100

    feat: add bombsite stats

--- metrics/maps.go
+++ metrics/maps.go
@@ -26,16 +26,17 @@ func SendMapStats(api *r6api.R6API, profile *r6api.Profile, meta *metadata.Metad
 
 	for gameModeName, gameModeStats := range gameModes {
 		for mapName, mapStats := range gameModeStats {
+			labels := map[string]string{
+				"season_slug": currentSeason.Slug,
+				"season_name": currentSeason.Name,
+				"username":    profile.Name,
+				"gamemode":    gameModeName,
+				"map":         mapName,
+			}
 			chData <- StatResponse{
 				P: influxdb2.NewPoint(
 					"maps",
-					map[string]string{
-						"season_slug": currentSeason.Slug,
-						"season_name": currentSeason.Name,
-						"username":    profile.Name,
-						"gamemode":    gameModeName,
-						"map":         mapName,
-					},
+					labels,
 					map[string]interface{}{
 						"matches_played":          mapStats.MatchesPlayed,
 						"matches_won":             mapStats.MatchesWon,
@@ -74,8 +75,69 @@ func SendMapStats(api *r6api.R6API, profile *r6api.Profile, meta *metadata.Metad
 					t,
 				),
 			}
-
+			if mapStats.Bombsites != nil {
+				sendMapBombsiteStats(mapStats.Bombsites, chData, labels, t)
+			}
 		}
 	}
 	chData <- StatResponse{Done: true}
+}
+
+func sendMapBombsiteStats(s *stats.BombsiteGamemodeStats, chData chan<- StatResponse, srcLabels map[string]string, t time.Time) {
+	teamRoles := map[string][]stats.BombsiteTeamRoleStats{
+		"all":     s.All,
+		"attack":  s.Attack,
+		"defence": s.Defence,
+	}
+
+	for teamRoleName, teamRole := range teamRoles {
+		for _, bombsiteStats := range teamRole {
+			labels := make(map[string]string, len(srcLabels))
+			for k, v := range srcLabels {
+				labels[k] = v
+			}
+			labels["role"] = teamRoleName
+			labels["bombsite"] = bombsiteStats.Name
+
+			chData <- StatResponse{
+				P: influxdb2.NewPoint(
+					"bombsites",
+					labels,
+					map[string]interface{}{
+						"kills":                   bombsiteStats.Kills,
+						"deaths":                  bombsiteStats.Deaths,
+						"assists":                 bombsiteStats.Assists,
+						"melee_kills":             bombsiteStats.MeleeKills,
+						"team_kills":              bombsiteStats.TeamKills,
+						"trades":                  bombsiteStats.Trades,
+						"revives":                 bombsiteStats.Revives,
+						"headshots":               bombsiteStats.Headshots,
+						"rounds_played":           bombsiteStats.RoundsPlayed,
+						"rounds_won":              bombsiteStats.RoundsWon,
+						"rounds_lost":             bombsiteStats.RoundsLost,
+						"minutes_played":          bombsiteStats.MinutesPlayed,
+						"kills_per_round":         bombsiteStats.KillsPerRound,
+						"headshot_percentage":     bombsiteStats.HeadshotPercentage,
+						"entry_deaths":            bombsiteStats.EntryDeaths,
+						"entry_death_trades":      bombsiteStats.EntryDeathTrades,
+						"entry_kills":             bombsiteStats.EntryKills,
+						"entry_kill_trades":       bombsiteStats.EntryKillTrades,
+						"rounds_survived":         bombsiteStats.RoundsSurvived,
+						"rounds_with_kill":        bombsiteStats.RoundsWithKill,
+						"rounds_with_multikill":   bombsiteStats.RoundsWithMultikill,
+						"rounds_with_ace":         bombsiteStats.RoundsWithAce,
+						"rounds_with_clutch":      bombsiteStats.RoundsWithClutch,
+						"rounds_with_kost":        bombsiteStats.RoundsWithKOST,
+						"rounds_with_entry_death": bombsiteStats.RoundsWithEntryDeath,
+						"rounds_with_entry_kill":  bombsiteStats.RoundsWithEntryKill,
+						"distance_per_round":      bombsiteStats.DistancePerRound,
+						"distance_total":          bombsiteStats.DistanceTotal,
+						"time_alive_per_match":    bombsiteStats.TimeAlivePerMatch,
+						"time_dead_per_match":     bombsiteStats.TimeDeadPerMatch,
+					},
+					t,
+				),
+			}
+		}
+	}
 }
