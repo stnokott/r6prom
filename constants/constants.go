cd3f83223223768022b82fc0504dd3d6acb45bf0
stnokott <nftrash@web.de>
2023-01-20 09:49:12 +0100

    build: use ldflags for setting version

--- constants/constants.go
+++ constants/constants.go
@@ -1,11 +1,11 @@
 package constants
 
-const (
-	NAME       string = "R6Influx"
-	VERSION    string = "v0.7.0"
-	USER_AGENT string = NAME + "/" + VERSION + " (see github.com/stnokott/r6prom)"
-)
+const NAME string = "R6Influx"
 
 // 0 = Debug, 1 = Info etc.
 // Is set to Info in goreleaser ldflags. Needs to be var of type string for ldflags to work.
-var LOG_LEVEL string = "0"
+var (
+	LOG_LEVEL  string = "0"
+	VERSION    string = "v0.7.0"
+	USER_AGENT string = NAME + "/" + VERSION + " (see github.com/stnokott/r6prom)"
+)
